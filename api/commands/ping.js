70dff3d51452a888bc40e5e7908fe5c39dc9acf2
ktsuench <will289@hotmail.com>
2021-07-07 00:03:30 -0400

    set up for digital ocean instead

--- api/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-    name: 'ping',
-    description: "This is a ping command!",
-    execute(message,args){
-        //write my code here for execution
-        message.channel.send('pong');
-    }
-}
